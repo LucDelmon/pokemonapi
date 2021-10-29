db1432e33d474062f24359de9b557d231ba4aab3
LucDelmon <35261367+LucDelmon@users.noreply.github.com>
2021-10-29 14:53:01 +0200

    feat: add seedings
    seedings is made from the provides csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'pokemon.csv'))
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  Pokemon.create!(
+    number: row['#'].to_i,
+    name: row['Name'],
+    type1: row['Type 1'],
+    type2: row['Type 2'],
+    total: row['Total'].to_i,
+    hp: row['HP'].to_i,
+    attack: row['Attack'].to_i,
+    defense: row['Defense'].to_i,
+    sp_attack: row['Sp. Atk'].to_i,
+    sp_defence: row['Sp. Def'].to_i,
+    speed: row['Speed'].to_i,
+    generation: row['Generation'].to_i,
+    legendary: ActiveRecord::Type::Boolean.new.cast(row['Legendary']),
+  )
+end
